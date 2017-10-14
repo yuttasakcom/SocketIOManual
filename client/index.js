7a2a66bf4e6bc7ce7cd704a2339e35949e50e7f1
YoProgrammer <yuttasakcom@gmail.com>
2017-10-14 18:22:41 +0700

    Create Nodejs

--- client/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Routes from './routes'
-import '@/assets/scss/mystyle.scss'
-
-ReactDOM.render(<Routes />, document.getElementById('app'))
--- client/vue.config.js
+++
@@ -1,5 +0,0 @@
-import Vue from 'react-vue'
-import Vuex from 'vuex'
-Vue.use(Vuex)
-
-export default Vue
